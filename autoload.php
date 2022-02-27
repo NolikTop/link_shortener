003d4c4c0a440817d197c92afa9ad20a3af8acfb
NolikTop <aydansuper12@gmail.com>
2022-02-28 01:02:16 +0300

    fix: пофиксил htaccess и autoload

--- autoload.php
+++ autoload.php
@@ -3,5 +3,7 @@
 declare(strict_types=1);
 
 spl_autoload_register(function (string $className): void{
+	$className = str_replace("\\", "/", $className);
+
 	include __DIR__ . "/src/$className.php";
 });
